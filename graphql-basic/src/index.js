09922ecb99b07f017c7d6dd17b3282519710ac5d
이상민 <>
2020-02-04 00:35:06 +0900

    update graphQL

--- graphql-basic/src/index.js
+++ graphql-basic/src/index.js
@@ -1,10 +1,56 @@
-import mylocation,{getGreeting,message,name} from './myModule'
-import defaultadd,{mysubstract} from './math'
+import {GraphQLServer} from 'graphql-yoga'
 
-console.log(mylocation)
-console.log(message)
-console.log(name)
-console.log(getGreeting('Jessica'))
+//Scalar types - String, Boolean, Int, Float, ID
 
-console.log(defaultadd(1,2))
-console.log(mysubstract(3,2))
+//Type definitions (schema)
+const typeDefs = `
+    type Query {
+        me: User!
+        post: Post!
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
+    }
+`
+
+//Resolver
+const resolvers = {
+    Query:{
+        me(){
+            return{
+                id: '123921',
+                name: 'LSM',
+                email: 'sd50712321@naver.com',
+                age: 31
+            }
+        },
+        post(){
+            return{
+                id: '394921',
+                title: 'love and peace',
+                body: 'hi is body',
+                published: true
+            }
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(()=>{
+    console.log('Ther server is up!')
+})
